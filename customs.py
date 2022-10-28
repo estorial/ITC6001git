ada9464b7069b808e291109fed0cc6ad50181031
Estorial <estorial34@gmail.com>
2022-10-28 18:41:58 +0300

    Changed encoding to iso-8859-1

--- customs.py
+++
@@ -1,4 +0,0 @@
-def CheckEntries(shapeA, shapeB):
-    out = shapeA - shapeB
-    return out
-
--- test1.py
+++ test1.py
@@ -1,6 +1,5 @@
 import pandas as pd
 from matplotlib import pyplot as plt
-from customs import CheckEntries
 from contextlib import redirect_stderr
 import re
 
@@ -9,29 +8,31 @@ book = 'Data/BX-Books.csv'
 users = 'Data/BX-Users.csv'
 
 # Load the files into dataframes.
-df_rat = pd.read_csv(file_ratings, sep=';', encoding='unicode_escape')
-# df_books = pd.read_csv(book, sep=';', encoding='unicode_escape', on_bad_lines='warn', low_memory=False)  # Some entries are malformed.
-df_users = pd.read_csv(users, sep=';', encoding='unicode_escape')
+df_rat = pd.read_csv(file_ratings, sep=';', encoding='iso-8859-1')
+# Some entries are malformed.
+# df_books = pd.read_csv(book, sep=';', encoding='unicode_escape', on_bad_lines='warn', low_memory=False)
+df_users = pd.read_csv(users, sep=';', encoding='iso-8859-1')
 
 # The next code block will attempt to find the ISBNs of the books that are malformed.
 # First redirect the error log to a file.
+
 with open('mal.txt', 'w') as h:
     with redirect_stderr(h):
-        df_books = pd.read_csv(book, sep=';', encoding='unicode_escape', on_bad_lines='warn', low_memory=False)
+        df_books = pd.read_csv(book, sep=';', encoding='iso-8859-1', on_bad_lines='warn', low_memory=False)
 
 temp_list = []
+
 # Next read the file and identify the lines using regex.
 # Temp_list contains the lines in the BX-Books.csv that are malformed.
 
 with open('mal.txt', 'r') as f:
     for line in f:
+    # The following if-else statements goes around the index error created from the last line containing only /n char
         if re.findall('\d+:', line):
             line_num = re.findall('\d+:', line)[0]
         else:
             continue
         temp_list.append(line_num[:-1])
-    print(temp_list)
-
 
 # Drop the last columns with the image URLs from book data frame.
 df_books.drop(['Image-URL-S', 'Image-URL-M', 'Image-URL-L'], axis=1, inplace=True)  # or assign it to new DF
